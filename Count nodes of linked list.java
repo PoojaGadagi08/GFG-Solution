28d07b421c58da2d01a7b9bb62cc2675e5813344
skya3 <107419125+PoojaGadagi08@users.noreply.github.com>
2023-05-22 19:11:29 +0530

    Create Count nodes of linked list.java

---
+++ Count nodes of linked list.java
@@ -0,0 +1,35 @@
+Given a singly linked list. The task is to find the length of the linked list, where length is defined as the number of nodes in the linked list.
+
+Example 1:
+
+Input:
+LinkedList: 1->2->3->4->5
+Output: 5
+Explanation: Count of nodes in the 
+linked list is 5, which is its length.
+  
+  
+  /*
+class Node{
+    int data;
+    Node next;
+    Node(int a){  data = a; next = null; }
+}*/
+
+class Solution
+{
+    //Function to count nodes of a linked list.
+    public static int getCount(Node head)
+    {
+        
+        //Code here
+        Node last=head;
+        int c=0;
+        while(last.next!=null){
+            c++;
+            last=last.next;
+        }
+        return c+1;
+       
+    }
+}
