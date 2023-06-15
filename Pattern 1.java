bacfe972ec906df2f4fb876d918afd0ffd26c576
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-06-15 10:55:48 +0530

    Create Pattern 1.java

---
+++ Pattern 1.java
@@ -0,0 +1,31 @@
+Geek is very fond of patterns. Once, his teacher gave him a square pattern to solve. He gave Geek an integer n and asked him to build a pattern.
+
+Help Geek to build a star pattern.
+
+ 
+
+Example 1:
+
+Input: 5
+
+Output:
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+
+
+class Solution {
+
+    void printSquare(int n) {
+        // code here
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
