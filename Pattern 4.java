ee4bcb4fb218c6ea57be1d2ae11190540721b2f0
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-06-15 11:54:41 +0530

    Create Pattern 4.java

---
+++ Pattern 4.java
@@ -0,0 +1,36 @@
+Geek is very fond of patterns. Once, his teacher gave him a  pattern to solve. He gave Geek an integer n and asked him to build a pattern.
+
+Help Geek to build a pattern.
+
+ 
+
+Example 1:
+
+Input: 5
+
+Output:
+1
+2 2 
+3 3 3 
+4 4 4 4 
+5 5 5 5 5
+
+
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+         int a=1;
+        for(int i=0;i<n;i++){
+           
+            for(int j=0;j<=i;j++){
+                System.out.print(a+" ");
+               
+            }
+             a+=1;
+             System.out.println();
+        }
+        
+    }
+}
