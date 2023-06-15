805e80cd74418fbd19c68641a178bababb27b964
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-06-15 11:38:41 +0530

    Create Pattern 2.java

---
+++ Pattern 2.java
@@ -0,0 +1,32 @@
+Geek is very fond of patterns. Once, his teacher gave him a pattern to solve. He gave Geek an integer n and asked him to build a pattern.
+
+Help Geek to build a star pattern.
+
+ 
+
+Example 1:
+
+Input: 5
+
+Output:
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+
+
+
+class Solution {
+
+    void printTriangle(int n) {
+        // code herefor
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
