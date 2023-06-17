565912a60ca479c35345e7a2710586bf8f158833
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-06-17 11:32:03 +0530

    Create Print N to 1 without loop.java

---
+++ Print N to 1 without loop.java
@@ -0,0 +1,24 @@
+Print numbers from N to 1 (space separated) without the help of loops.
+
+Example 1:
+
+Input:
+N = 10
+Output: 10 9 8 7 6 5 4 3 2 1
+  
+  
+  
+  
+class Solution {
+
+    void printNos(int N) {
+        // code here
+        // if(N>0){
+        //     System.out.print(N+" ");
+        //     printNos(N-1);
+        // }
+        if(N==0) return ;
+         System.out.print(N+" ");
+         printNos(N-1);
+    }
+}
