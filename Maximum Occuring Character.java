c265edbf67a93ab9eda7815c82fe6d80932887e6
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-07-14 13:41:43 +0530

    Create Maximum Occuring Character.java

---
+++ Maximum Occuring Character.java
@@ -0,0 +1,64 @@
+Given a string str of lowercase alphabets. The task is to find the maximum occurring character in the string str. If more than one character occurs the maximum number of time then print the lexicographically smaller character.
+
+Example 1:
+
+Input:
+str = testsample
+Output: e
+Explanation: e is the character which
+is having the highest frequency.
+Example 2:
+
+Input:
+str = output
+Output: t
+Explanation:  t and u are the characters
+with the same frequency, but t is
+lexicographically smaller.
+Your Task:
+The task is to complete the function getMaxOccuringChar() which returns the character which is most occurring.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Number of distinct characters).
+Note: N = |s|
+
+Constraints:
+1 ≤ |s| ≤ 100
+
+
+
+
+
+
+  
+class Solution
+{
+    //Function to find the maximum occurring character in a string.
+     public static char getMaxOccuringChar(String s)
+    {
+        Map<Character,Integer> map = new HashMap<>();
+        for(char c : s.toCharArray()){
+            if(!map.containsKey(c)){
+                map.put(c,1);
+            }else{
+                map.put(c, map.get(c)+1);
+            }
+        }
+        int max = 0;
+       for(Map.Entry<Character,Integer> entry : map.entrySet()){
+           if(entry.getValue() > max){
+               max = entry.getValue();
+           }
+       }
+
+       List<Character> list = new ArrayList<>();
+        for(Map.Entry<Character,Integer> entry : map.entrySet()){
+            if(entry.getValue() == max){
+              list.add(entry.getKey());
+            }
+        }
+        Collections.sort(list);
+        return list.get(0);
+    }
+    
+}
