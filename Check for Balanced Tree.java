cb410a205f5cbfc56431e99b146bfa934e9162d6
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2023-09-02 19:45:47 +0530

    Create Check for Balanced Tree.java

---
+++ Check for Balanced Tree.java
@@ -0,0 +1,100 @@
+Given a binary tree, find if it is height balanced or not. 
+A tree is height balanced if difference between heights of left and right subtrees is not more than one for all nodes of tree. 
+
+A height balanced tree
+        1
+     /     \
+   10      39
+  /
+5
+
+An unbalanced tree
+        1
+     /    
+   10   
+  /
+5
+
+Example 1:
+
+Input:
+      1
+    /
+   2
+    \
+     3 
+Output: 0
+Explanation: The max difference in height
+of left subtree and right subtree is 2,
+which is greater than 1. Hence unbalanced
+Example 2:
+
+Input:
+       10
+     /   \
+    20   30 
+  /   \
+ 40   60
+Output: 1
+Explanation: The max difference in height
+of left subtree and right subtree is 1.
+Hence balanced. 
+Your Task:
+You don't need to take input. Just complete the function isBalanced() that takes root node as parameter and returns true, if the tree is balanced else returns false.
+
+Constraints:
+1 <= Number of nodes <= 105
+0 <= Data of a node <= 106
+
+Expected time complexity: O(N)
+Expected auxiliary space: O(h) , where h = height of tree
+
+
+
+
+
+  
+//User function Template for Java
+
+
+/* A binary tree node class
+class Node
+{
+	int data;
+	Node left,right;
+	
+	Node(int d)
+	{
+		data = d;
+		left = right = null;		
+	}
+} */
+
+class Tree
+{
+    
+    public int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+    
+    boolean isBalanced(Node root)
+    {
+         if (root == null) {
+            return true; // An empty tree is balanced
+        }
+	    int lh = height(root.left);
+	    int rh = height(root.right);
+	    int diff = Math.abs(lh - rh);
+	    
+	    if(diff<=1){
+	          // Recursively check balance for left and right subtrees
+            return isBalanced(root.left) && isBalanced(root.right);
+	    }
+	    return false;
+    }
+}
