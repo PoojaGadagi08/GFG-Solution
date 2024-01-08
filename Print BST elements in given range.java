65b1c41e7f62f533d5f0872ae63e9ab71ee2e028
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2024-01-08 20:24:05 +0530

    Create Print BST elements in given range.java

---
+++ Print BST elements in given range.java
@@ -0,0 +1,63 @@
+Given a Binary Search Tree and a range [low, high]. Find all the numbers in the BST that lie in the given range.
+Note: Element greater than or equal to root go to the right side.
+
+Example 1:
+
+Input:
+       17
+     /    \
+    4     18
+  /   \
+ 2     9 
+l = 4, h = 24
+Output: 4 9 17 18 
+Example 2:
+
+Input:
+       16
+     /    \
+    7     20
+  /   \
+ 1    10
+l = 13, h = 23
+Output: 16 20 
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function printNearNodes() which takes the root Node of the BST and the range elements low and high as inputs and returns an array that contains the BST elements in the given range low to high (inclusive) in non-decreasing order.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(Height of the BST).
+
+Constraints:
+1 ≤ Number of nodes ≤ 104
+1 ≤ l ≤ h ≤ 105
+
+
+
+
+  class Solution
+{   
+    //Function to return a list of BST elements in a given range.
+	public static ArrayList<Integer> printNearNodes(Node root,int low,int high) {
+        // code here.
+        ArrayList<Integer> arr =new ArrayList<>();
+        fun(root, low, high, arr);
+        
+        return arr;
+        
+    }
+    
+    public static void fun(Node root, int low, int high, ArrayList<Integer> arr){
+        if(root == null)
+        return ;
+        
+        if(root.data >= low)
+        fun(root.left, low, high, arr);
+        
+        if(root.data >= low && root.data<=high)
+        arr.add(root.data);
+        
+        if(root.data <= high)
+        fun(root.right, low, high, arr);
+    }
+    
+}
