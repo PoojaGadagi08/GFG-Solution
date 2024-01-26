3e052019186a6947e29ddfdb26e9723646db2cbd
Pooja Gadagi <107419125+PoojaGadagi08@users.noreply.github.com>
2024-01-26 11:29:49 +0530

    Create Word ladder - 1.java

---
+++ Word ladder - 1.java
@@ -0,0 +1,112 @@
+Given two distinct words startWord and targetWord, and a list denoting wordList of unique words of equal lengths. Find the length of the shortest transformation sequence from startWord to targetWord.
+Keep the following conditions in mind:
+
+A word can only consist of lowercase characters.
+Only one letter can be changed in each transformation.
+Each transformed word must exist in the wordList including the targetWord.
+startWord may or may not be part of the wordList
+The second part of this problem can be found here.
+
+Note: If no possible way to transform sequence from startWord to targetWord return 0
+
+
+Example 1:
+
+Input:
+wordList = {"des","der","dfr","dgt","dfs"}
+startWord = "der", targetWord= "dfs",
+Output:
+3
+Explanation:
+The length of the smallest transformation
+sequence from "der" to "dfs" is 3
+i,e "der" -> "dfr" -> "dfs".
+Example 2:
+
+Input:
+wordList = {"geek", "gefk"}
+startWord = "gedk", targetWord= "geek", 
+Output:
+2
+Explanation:
+gedk -> geek
+Example 3:
+
+Input: 
+wordList = {"poon", "plee", "same", "poie","plea","plie","poin"}
+startWord = "toon", targetWord= "plea",
+Output: 7 
+Explanation:
+toon -> poon -> poin -> poie -> plie -> plee -> plea 
+ 
+
+Your Task:
+You don't need to read or print anything, Your task is to complete the function wordLadderLength() which takes startWord, targetWord and wordList as input parameter and returns the length of the shortest transformation sequence from startWord to targetWord. If not possible return 0.
+
+
+Expected Time Compelxity: O(N2 * M)
+Expected Auxiliary Space: O(N * M) where N = length of wordList and M = |wordListi|
+
+
+Constraints:
+1 ≤ N ≤ 100
+1 ≤ M ≤ 10
+
+
+
+
+  class Pair {
+    String first;
+    int second;
+    Pair(String _first, int _second) {
+        this.first = _first;
+        this.second = _second;
+    }
+}
+class Solution {
+    public int wordLadderLength(String startWord, String targetWord, String[] wordList) {
+        // Creating a queue ds of type {word,transitions to reach ‘word’}.
+        Queue < Pair > q = new LinkedList < > ();
+ 
+        // BFS traversal with pushing values in queue 
+        // when after a transformation, a word is found in wordList.
+        q.add(new Pair(startWord, 1));
+
+        // Push all values of wordList into a set
+        // to make deletion from it easier and in less time complexity.
+        Set < String > st = new HashSet < String > ();
+        int len = wordList.length;
+        for (int i = 0; i < len; i++) {
+            st.add(wordList[i]);
+        }
+        st.remove(startWord);
+        while (!q.isEmpty()) {
+            String word = q.peek().first;
+            int steps = q.peek().second;
+            q.remove();
+      
+            // we return the steps as soon as
+            // the first occurence of targetWord is found.
+            if (word.equals(targetWord) == true) return steps;
+
+            // Now, replace each character of ‘word’ with char
+            // from a-z then check if ‘word’ exists in wordList.
+            for (int i = 0; i < word.length(); i++) {
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    char replacedCharArray[] = word.toCharArray();
+                    replacedCharArray[i] = ch;
+                    String replacedWord = new String(replacedCharArray);
+
+                    // check if it exists in the set and push it in the queue.
+                    if (st.contains(replacedWord) == true) {
+                        st.remove(replacedWord);
+                        q.add(new Pair(replacedWord, steps + 1));
+                    }
+                }
+
+            }
+        }
+        // If there is no transformation sequence possible
+        return 0;
+    }
+}
